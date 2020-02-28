93b0321f042c5ac00d3b19cf586000683a823d6c
鍾念庭 <nickchunglolz@zhongniantingde-MacBook-Pro.local>
2020-02-29 00:30:38 +0800

    vue lazy load optimization

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import router from './Router'
 import VueLazyload from 'vue-lazyload'
 
-Vue.use(VueLazyload);
+Vue.use(VueLazyload,{
+  preLoad:1.3
+});
 
 Vue.config.productionTip = false
 
