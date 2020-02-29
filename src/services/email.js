c109842732b5cb23c9d18136aadd71731b3c562a
鍾念庭 <nickchunglolz@zhongniantingde-MacBook-Pro.local>
2020-03-01 04:47:56 +0800

    Form Validation Added.

--- src/services/email.js
+++ src/services/email.js
@@ -2,15 +2,24 @@
 export default {
     data(){
         return {
-            emailBody: {}
+            emailBody: {},
+            reg: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/
         }
     },
     methods: {
         async sendMail(body){
                 try{
-                    let res = await this.$axios.post('/email',body);
-                    alert("Send mail successfully.");
-                    return res.data;
+                    if(this.reg.test(body.email) && body.userName && body.title && body.content){
+                        let res = await this.$axios.post('/email',body);
+                        alert("Send mail successfully.");
+                        return res.data;
+                    }else{
+                        if(!this.reg.test(body.email)){
+                            alert("ERROR：Email Format Error!!");
+                        }else{
+                            alert("ERROR：Fill The Form Plz!!");
+                        }
+                    }
                 }catch(e){
                     console.log(e);
                 }
