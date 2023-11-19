7d57284644aac8b3eddb57cc98b3aadab66c85dd
Nick Chung <nickchunglolz@zhongniantingde-MacBook-Pro-6303.local>
2023-11-19 13:26:56 +0800

    Update: Removed eslint.

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="padding-0" id="app">
-    <NavBar/>
+    <NavBar />
     <div class="row">
-        <div class="row">
-          <Home class="HOME" id="HOME"/>
-        </div>
-        <div class="row" style="height:auto;">
-              <About class="ABOUT" id="ABOUT"/>
-        </div>
-        <div class="row" style="height:auto;">
-          <Tech class="TECH" id="TECH"/>
-        </div>
-        <div class="row" style="height:auto;">
-          <Portfolio class="PORT" id="PORT"/>
-        </div>
-        <div class="row" style="height:auto;">
-          <Experience class="EXPER" id="EXPER"/>
-        </div>
-        <div class="row">
-          <Contact class="CONTACT" id="CONTACT"/>
-        </div>
+      <div class="row">
+        <Home class="HOME" id="HOME" />
+      </div>
+      <div class="row" style="height: auto">
+        <About class="ABOUT" id="ABOUT" />
+      </div>
+      <div class="row" style="height: auto">
+        <Tech class="TECH" id="TECH" />
+      </div>
+      <div class="row" style="height: auto">
+        <Portfolio class="PORT" id="PORT" />
+      </div>
+      <div class="row" style="height: auto">
+        <Experience class="EXPER" id="EXPER" />
+      </div>
+      <div class="row">
+        <Contact class="CONTACT" id="CONTACT" />
+      </div>
     </div>
     <!-- Modal -->
     <!-- <ChatModal/> -->
@@ -29,21 +29,21 @@
 <script>
 /* eslint-disable */
 // layout
-const NavBar = () => import('./components/layout/NavBar.vue');
+const NavBar = () => import("./components/layout/NavBar.vue");
 // pages
-const Home = () => import('./components/Home.vue');
-const About = () => import('./components/About.vue');
-const Tech = () => import('./components/Tech.vue');
-const Portfolio = () => import('./components/Portfolio.vue');
-const Experience = () => import('./components/Experience.vue');
-const Contact = () => import('./components/Contact.vue');
+const Home = () => import("./components/Home.vue");
+const About = () => import("./components/About.vue");
+const Tech = () => import("./components/Tech.vue");
+const Portfolio = () => import("./components/Portfolio.vue");
+const Experience = () => import("./components/Experience.vue");
+const Contact = () => import("./components/Contact.vue");
 // Modals
-const ChatModal = () => import('./components/Chat.vue');
+const ChatModal = () => import("./components/Chat.vue");
 //js
-const modal = () => import('./components/js/modal');
+const modal = () => import("./components/js/modal");
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     NavBar,
     Home,
@@ -52,12 +52,12 @@ export default {
     Portfolio,
     Experience,
     Contact,
-    ChatModal
+    ChatModal,
   },
-  beforeCreate(){
+  beforeCreate() {
     // modal();
-  }
-}
+  },
+};
 </script>
 
 <style src="./App.scss" lang="scss"/>
